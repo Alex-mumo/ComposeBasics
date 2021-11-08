2ec06ef54644005c561fec4d8b8e9ba14a95c13a
Alex-mumo <alexymumo2000@gmail.com>
2021-11-08 16:08:45 +0300

    Initial commit

--- app/src/main/java/com/example/composebasics/MainActivity.kt
+++ app/src/main/java/com/example/composebasics/MainActivity.kt
@@ -3,36 +3,54 @@ package com.example.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composebasics.ui.theme.ComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //compose functions here
         setContent {
             ComposeBasicsTheme {
+                App()
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                //Surface(color = MaterialTheme.colors.background) {
+                    //Greeting("Alex")
+
             }
         }
+    }
+
+
+}
+@Composable
+private fun App() {
+    Surface(color = MaterialTheme.colors.background){
+        Greeting("Alex")
+
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(25.dp))
+    }
 }
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = true, name = "Firstname")
 @Composable
 fun DefaultPreview() {
     ComposeBasicsTheme {
-        Greeting("Android")
+        App()
     }
 }
